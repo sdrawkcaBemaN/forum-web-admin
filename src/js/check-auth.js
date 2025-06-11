2ac1b563dffdef84f9431221d0b70e134f994b4b
sdrawkcabemaN <kinan.sutattyo@gmail.com>
2025-06-11 12:18:24 +0700

    refactor and clean up

--- src/js/check-auth.js
+++ src/js/check-auth.js
@@ -1,6 +1,6 @@
 import { getAuthenticatedUser } from "./api/auth.js";
 
-export async function checkAuth(eligibleRoles = []) {
+export async function checkAuth(eligibleRoles = [], status = ["verified"]) {
   const { data: user } = await getAuthenticatedUser();
   if (!user) {
     window.location.href = "./login.html";
@@ -9,6 +9,10 @@ export async function checkAuth(eligibleRoles = []) {
 
   if (user && !eligibleRoles.includes(user.role)) {
     window.location.href = "./login.html";
+    return null;
+  }
+
+  if (!user || !status.includes(user.status)) {
     return null;
   }
 
