f7398a30647e2a6fc361b27ffbdbc316f6ff5e58
sdrawkcabemaN <kinan.sutattyo@gmail.com>
2025-06-24 20:57:47 +0700

    ban/unban 24/6/25

---
+++ src/js/api/users.js
@@ -0,0 +1,83 @@
+export async function getUsers() {
+  try {
+    const response = await fetch("http://127.0.0.1:5000/user/", {
+      method: "GET",
+      credentials: "include",
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      return {
+        error: null,
+        data: result.data,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
+
+export async function banUser(id) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/user/${id}/banned`, {
+      method: "PATCH",
+      credentials: "include",
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      return {
+        error: null,
+        data: result.data,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
+
+
+export async function unbanUser(id) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/user/${id}/unbanned`, {
+      method: "PATCH",
+      credentials: "include",
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      return {
+        error: null,
+        data: result.data,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
